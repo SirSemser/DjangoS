c7a9cb96b72802176bf8181ba0905fceed353712
SirSemser <semserberdibek@gmail.com>
2024-04-01 17:56:06 +0600

    Отслежвание

--- main/views.py
+++ main/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("<h4>Семсер Бердибек</h4>")
+
+def about(request):
+    return HttpResponse("<h4>Страница про Семсер</h4>")

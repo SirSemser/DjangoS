64e1b1c1d5dc19325fb1f46a70f4410c9ad5383b
SirSemser <semserberdibek@gmail.com>
2024-04-22 18:05:50 +0600

    Это 10-11 лаб HTML и CSS закончено остолось Работа с базой данных

--- main/views.py
+++ main/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 # Create your views here.
 def index(request):
-    return render(request, 'main/index.html')
+    data = {
+        'title': 'Главная страница',
+        'values': ['Hello', 'Semser', 'space'],
+        'obj': {
+            'car': 'mers',
+            'age': 18,
+            'hobby': 'anime'
+        }
+    }
+    return render(request, 'main/index.html', data)
 
 def about(request):
     return render(request, 'main/about.html')
